4c2b3f4473ef3e3a2077a7a7dd53c519a2e712dc
pablomarcel <pablomarcel@outlook.com>
2022-02-17 20:14:54 -0800

    completed plusAndMinuses

--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,23 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
+let counter =0
+
+let incRef = document.getElementById('increment')
+
+let decRef = document.getElementById('decrement')
+
+let divRef = document.getElementById('counter')
+
+incRef.addEventListener('click', ()=>{
+    counter++;
+    console.log(counter)
+    divRef.innerText=counter;
+})
+
 // When a user clicks the – element, the count should decrease by 1 on screen.
+
+//
+decRef.addEventListener('click', ()=>{
+    counter--;
+    console.log(counter)
+    divRef.innerText=counter;
+})

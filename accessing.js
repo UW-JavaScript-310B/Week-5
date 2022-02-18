6cfa6a44df395c7d1be0c62f6e64e3ad3686f601
pablomarcel <pablomarcel@outlook.com>
2022-02-17 19:06:41 -0800

    completed accessing.html

--- accessing.js
+++ accessing.js
@@ -1,5 +1,17 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
 
+const weatherHeader = document.getElementById("weather-head")
+weatherHeader.textContent='February 10 Weather Forecast, Seattle'
+
+
 // Change the styling of every element with class "sun" to set the color to "orange"
 
+const sunClasses = document.querySelectorAll('.sun')
+
+sunClasses.forEach((thing)=>thing.style.color = 'orange')
+
 // Change the class of the second <li> from to "sun" to "cloudy"
+
+const secondSun = document.querySelectorAll('.sun')[1]
+
+secondSun.className='cloudy'

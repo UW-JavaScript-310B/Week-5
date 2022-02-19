578c8e876e97b49d2323ed2f24d076554f136cda
JCahill <cahilljm@aol.com>
2022-02-18 21:35:50 -0800

    Push accessing, dom-crud and plusesAndMinuses solutions

--- dom-crud.js
+++ dom-crud.js
@@ -2,13 +2,39 @@
 // with an id of "cta" after the last <p>
 
 
+// const aEl = document.createElement( "a" );
+
+// // aEl.innerHTML = "Buy Now!";
+// aEl.innerText = "Buy Now!";
+// aEl.setAttribute("id","cta");
+
+
+// const mainEl = document.getElementsByTagName( "main" );
+// aEl.appendChild(mainEl);
+
+const aEl = document.createElement('a');
+aEl.innerText = "Buy Now!";
+aEl.setAttribute("id","cta");
+const main = document.getElementsByTagName('main')[0];
+main.appendChild(aEl);
+
+
 // Access (read) the data-color attribute of the <img>,
 // log to the console
+const carImg = document.getElementsByTagName('img')[0];
+const carColor = carImg.getAttribute('data-color');
+console.log(carColor);
 
 
 // Update the third <li> item ("Turbocharged"), 
 // set the class name to "highlight"
-
+const listItem = document.getElementsByTagName('li')[2];
+listItem.classList.add('highlight');
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+let pEl = document.getElementsByTagName('p')[0];
+main.removeChild(pEl);
+
+
+
--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,66 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
 // When a user clicks the – element, the count should decrease by 1 on screen.
+
+
+//create a main element inside the body for the counter and buttons
+let mainEl = document.createElement("main")
+mainEl.setAttribute("id", "main-section");
+
+// Get the reference element for the script element
+let scriptEl = document.getElementById("lastEl")
+
+// Get the parent element
+let parentEl = scriptEl.parentNode
+
+// Insert the new main element into the body before the script
+parentEl.insertBefore(mainEl, scriptEl)
+
+
+//create a button for adding
+const addButton = document.createElement('button');
+addButton.setAttribute("id", "add");
+addButton.innerHTML = "+";
+mainEl.appendChild(addButton);
+
+
+//create a button for subtracting
+const subtractButton = document.createElement('button');
+subtractButton.setAttribute("id", "subtract");
+subtractButton.innerHTML = "-";
+mainEl.appendChild(subtractButton);
+
+
+//create a div for the counter and assign it an id of "counter"
+const divEl = document.createElement('div');
+divEl.setAttribute("id", "counter");
+mainEl.appendChild(divEl);
+
+//add counter to the page
+let counter = 0;
+divEl.innerHTML = `Total Count is ${counter}`;
+
+//create event to add a 1 when add button is clicked
+const addEl = document.getElementById('add');
+
+addEl.addEventListener('click', () => {
+    {
+        counter++;
+    }
+    console.log(counter);
+    divEl.innerHTML = `Total Count is ${counter}`;
+
+
+})
+
+//create event to subtrace a 1 when subtract button is clicked
+
+const subtractEl = document.getElementById('subtract');
+
+subtractEl.addEventListener('click', () => {
+    {
+        counter--;
+    }
+    console.log(counter);
+    divEl.innerHTML = `Total Count is ${counter}`;
+
+})

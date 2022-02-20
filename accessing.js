9ec847791dc495c44628bab1f91cabfda6748d55
jl105 <jilinda10@gmail.com>
2022-02-20 13:36:43 -0800

    Yuhong Solution to Week 5 Assignment

--- accessing.js
+++ accessing.js
@@ -1,5 +1,9 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
-
+document.getElementById("weather-head").innerHTML = "February 10 Weather Forecast, Seattle";
 // Change the styling of every element with class "sun" to set the color to "orange"
-
+let els = document.getElementsByClassName("sun");
+Array.prototype.forEach.call(els, function(el) {
+    el.style.color  = "orange";
+});
 // Change the class of the second <li> from to "sun" to "cloudy"
+document.getElementsByClassName("sun")[1].className = "cloudy";
--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,14 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
+let counter = 0;
+const plusEl = document.getElementById("btnInc");
+const minusEl = document.getElementById("btnDec");
+let cntTextEl = document.getElementById("cntText");
+plusEl.addEventListener ('click', (e) => {
+    counter++;
+    cntTextEl.innerHTML = `Count is ${counter}`;
+});
+minusEl.addEventListener ('click', (e) => {
+    counter--;
+    cntTextEl.innerHTML = `Count is ${counter}`;
+});
 // When a user clicks the – element, the count should decrease by 1 on screen.

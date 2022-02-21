113f65b2db7d51db1af203059878a15a6065d7d2
JCahill <cahilljm@aol.com>
2022-02-20 17:17:17 -0800

    Push latest changes to stoppingbehavior and todo activities

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -1,54 +1,37 @@
-const meowEl = document.getElementById('cat');
-
-
-const removeMeow = function () {
-  document.getElementById('cat').removeEventListener( "click", () => {alert('meow!');
+// Do not change
+document.getElementById('cat').addEventListener('click', (e) => {
+  alert('meow!');
 });
-};
-
-const addMeow = function () {
-  document.getElementById('cat').addEventListener('click', () => {alert('meow!');
-  });
-  
-};
-
 
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
 
-
-
+document.getElementById("more-info").addEventListener('click', () => {
+  e.preventDefault();
+  alert("Here's some info");
+});
 
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
 
-const addBark = function () {
-  removeMeow();
-  document.getElementById('dog').addEventListener('click', () => {alert('Bow Wow');
+
+// document.getElementById('cat').removeEventListener('click', () => {
+//   alert('meow!');
+// });
+
+// removeMeow = function () {
+//   document.getElementById('cat').removeEventListener('click', () => {
+//     alert('meow!');
+//   });
+// }
+
+document.getElementById('dog').addEventListener('click', () => {
+  // removeMeow();
+  document.getElementById('cat').removeEventListener('click', () => {
+    alert('meow!');
+  });
+  alert('Bow Wow!');
 
 });
-
-};
-
-
-
-
-
-
-
-
-// betterToDos[0]
-// .removeEventListener('click', toggleToDo);
-
-// const myDiv = document.getElementById("myDIV");
-// myDiv.addEventListener("mousemove", myFunction);
-
-// function removeHandler() {
-//   myDiv.removeEventListener("mousemove", myFunction);
-// }
-
-// function myFunction() {
-//   document.getElementById("demo").innerHTML = Math.random();
-// }
--- toDoList.js
+++ toDoList.js
@@ -1,14 +1,54 @@
 // If an li element is clicked, toggle the class "done" on the <li>
 
+
+  const list = document.getElementsByClassName('today-list');
+  const listItems = document.getElementsByTagName('li')[0];
+  // return listItems;
+  
+ let done =  listItems.addEventListener('click', e => {
+    let li = e.target;
+    if (li.getAtrribute('class') !== "done") {
+      li.setAttribute('class', "done");
+    } else {
+    }
+    });
+
+
+  // const betterToDos = 
+  // document.querySelectorAll('#better-to-do li');
+
+
+
+// for (let i = 0; i < tasks.children.length; i++) {
+//    if taskListClass[i].getAttribute("class") !== 'done' {
+//      task
+//    }
+
+
+
+
+// bestToDoList.addEventListener('click', e => {
+// let li = e.target;
+// if (li.innerHTML.indexOf('□') !== -1) {
+// li.innerHTML = li.innerHTML.replace('□',  '✓');
+// } else {
+// li.innerHTML = li.innerHTML.replace('✓',  '□');
+// }
+// });
+
+
+
 // If a delete link is clicked, delete the li element / remove from the DOM
 
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
 // Make sure to add an event listener(s) to your new <li> (if needed)
-const addListItem = function(e) {
-  e.preventDefault();
-  const input = this.parentNode.getElementsByTagName('input')[0];
-  const text = input.value; // use this text to create a new <li>
+
+
+// const addListItem = function(e) {
+//   e.preventDefault();
+//   const input = this.parentNode.getElementsByTagName('input')[0];
+//   const text = input.value; // use this text to create a new <li>
 
   // Finish function here
-};
+// };

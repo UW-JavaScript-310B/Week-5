750b9f60c69a2cd62032caf39d2ca7dc1c532b07
RainyCityDiver <cvmembership@posteo.net>
2022-02-22 14:19:08 -0800

    traversing.js code tested and working.

--- traversing.js
+++ traversing.js
@@ -1,11 +1,21 @@
 // Given the <body> element as variable body,
 // access the <main> node and log to the console.
 const body = document.querySelector('body');
+let bodyChildNodes = body.childNodes;
+let mainNode = bodyChildNodes[1];
+console.log(mainNode);
 
 // Given the <ul> element as variable ul,
 // access <body>  and log to the console.
 const ul = document.querySelector('ul');
+let parentOne = ul.parentNode;
+let parentTwo = parentOne.parentNode;
+console.log(parentTwo);
 
 // Given the <p> element as var p,
 // access the 3rd <li>  and log to the console.
 const p = document.querySelector('p');
+let prevSibUl = p.previousElementSibling;
+let ulChildren = prevSibUl.childNodes;
+let thirdListItem = ulChildren[5];
+console.log(thirdListItem);

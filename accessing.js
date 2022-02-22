899abc2a6b33738cbb9cab118603de35394ecf0a
Nell B <nellb28@yahoo.com>
2022-02-22 06:34:37 -0600

    add latest version of assignment

--- accessing.js
+++ accessing.js
@@ -1,5 +1,24 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
+const weatherHeader = document.getElementById("weather-head");
+weatherHeader.innerHTML = "February 10 Weather Forecast, Seattle";
+weatherHeader.addEventListener("click");
 
 // Change the styling of every element with class "sun" to set the color to "orange"
+const sunsByGet = document.getElementsByClassName("sun");
+for (let index = 0; index < sunsByGet.length; index++) {
+  sunsByGet.item(index).style.color = "blue";
+}
+
+// const suns = document.querySelectorAll(".sun");
+// suns.forEach((element) => {
+//   element.style.color = "orange";
+// });
 
 // Change the class of the second <li> from to "sun" to "cloudy"
+//jquery
+// const allSunElements = document.querySelectorAll(".sun");
+// allSunElements[1].className = "cloudy";
+const secondSunElement = document.querySelector("ul li")[1];
+secondSunElement.className = "cloudy";
+//TODO figure out how to get JQuery to work for nth element
+//SHould be similar to this const thirdListItem = document.querySelector("ul li:nth-child(2)");
--- dom-crud.js
+++ dom-crud.js
@@ -1,14 +1,24 @@
-// Create a new <a> element containing the text "Buy Now!" 
+// Create a new <a> element containing the text "Buy Now!"
 // with an id of "cta" after the last <p>
-
+const aTag = document.createElement("a");
+aTag.setAttribute("id", "cta");
+aTag.innerText = "Buy Now!";
+const pTag = document.getElementsByTagName("p")[0];
+pTag.appendChild(aTag);
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
+const imageTag = document.getElementsByTagName("img")[0];
+const dataColor = imageTag.dataset.color;
+console.log(dataColor);
 
-
-// Update the third <li> item ("Turbocharged"), 
+// Update the third <li> item ("Turbocharged"),
 // set the class name to "highlight"
-
+const thirdListElement = document.querySelector("ul li:nth-child(2)");
+thirdListElement.setAttribute("class", "highlight");
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+const mainElement = document.getElementsByTagName("main")[0];
+const paragraphElement = document.getElementByTagName("p")[0];
+mainElement.removeChild(paragraphElement);

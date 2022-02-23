9debb649b105b25bf4d1e63f4037141385a23515
RainyCityDiver <cvmembership@posteo.net>
2022-02-23 11:39:20 -0800

    plusesAndMinuses code tweaked, still working.

--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,7 +1,7 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
 // When a user clicks the – element, the count should decrease by 1 on screen.
-const plusOne = document.querySelector('button.btn-info');
-const minusOne = document.querySelector('button.btn-warning');
+const plusOne = document.getElementById('up');
+const minusOne = document.getElementById('down');
 const htmlCounter = document.querySelector('p.counter-counter');
 let counter = 0;
 plusOne.addEventListener('click', () => {

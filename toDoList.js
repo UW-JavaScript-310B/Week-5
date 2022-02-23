90d9fd897bc11cd30932bc65a0ec6ca99fade136
Arthur Rodrigues <artrodrig3@outlook.com>
2022-02-22 22:01:55 -0800

    Week 5 homework completed.

--- toDoList.js
+++ toDoList.js
@@ -1,12 +1,12 @@
 // If an li element is clicked, toggle the class "done" on the <li>
 document.addEventListener('click', e => {
-    if (e.target.tagName === 'SPAN' && e.target.parentNode.parentNode.className === 'today-list') {
+    if (e.target.tagName === 'SPAN' && e.target.closest('.today-list')) {
         e.target.parentNode.className = 'done';
     } 
 })
 // If a delete link is clicked, delete the li element / remove from the DOM
 document.addEventListener('click', e => {
-    if (e.target.className === 'delete' && e.target.parentNode.parentNode.className === 'today-list') {
+    if (e.target.className === 'delete' && e.target.closest('.today-list').className === 'today-list') {
         e.target.parentNode.remove();
     } 
 })
@@ -20,9 +20,25 @@ const addListItem = function(e) {
   const text = input.value; // use this text to create a new <li>
 
   // Finish function here
+    const todayList = document.querySelector('.today-list');
+    const listItem = document.createElement('li');
+    
+    const spanItem = document.createElement('span');
+    spanItem.textContent = text + ' ';
+
+    const anchorItem = document.createElement('a');
+    anchorItem.textContent = 'Delete'
+    anchorItem.setAttribute('class', 'delete');
+    
+    listItem.append(spanItem);
+    listItem.append(anchorItem);
+    todayList.append(listItem);
+
+
+    console.log('durrr');
 };
 
+
+// Lexical scoping--- look it up...
 const addItem = document.getElementsByClassName('add-item')[0];
-addItem.addEventListener('click', () => {
-    addListItem;
-})
+addItem.addEventListener('click', addListItem);

50faa9639f5d619e7830541c028ebe5814bfd98c
Sonal Gupta <sonalaggarwal555@gmail.com>
2022-02-22 23:56:32 -0800

    Week-5 Assignment Part-1

--- accessing.js
+++ accessing.js
@@ -1,5 +1,11 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
+const link = document.getElementById('weather-head');
+link.innerText = 'February 10 Weather Forecast, Seattle';
 
 // Change the styling of every element with class "sun" to set the color to "orange"
+const sunlink = document.querySelectorAll('.sun');
+sunlink.forEach((el)=> el.style.color = 'orange');
 
 // Change the class of the second <li> from to "sun" to "cloudy"
+var list = document.getElementsByTagName("li");
+list[2].className = 'cloudy';

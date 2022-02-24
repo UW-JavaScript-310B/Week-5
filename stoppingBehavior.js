0a33a2350a549b7766f499727bd64f9e4f168f30
Nell B <nellb28@yahoo.com>
2022-02-23 22:26:11 -0600

    add Stopping behavior job

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -1,10 +1,22 @@
 // Do not change
-document.getElementById('cat').addEventListener('click', () => {
-  alert('meow!');
+document.getElementById("cat").addEventListener("click", () => {
+  alert("meow!");
 });
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
+document.getElementById("more-info").addEventListener("click", clickAlert);
+
+function clickAlert(event) {
+  event.preventDefault();
+  alert("Here's some info");
+}
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+document.getElementById("dog").addEventListener("click", bark);
+
+function bark(event) {
+  event.stopPropagation();
+  alert("Bow wow!");
+}

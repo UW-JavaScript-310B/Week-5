77d8f0311efb9dfada631f6ead5cdd7b725886e4
JCahill <cahilljm@aol.com>
2022-02-24 15:28:53 -0800

    Push changes to toDoList assignment

--- accessing.js
+++ accessing.js
@@ -5,9 +5,9 @@ title.innerHTML = 'February 10 Weather Forecast, Seattle';
 
 // Change the styling of every element with class "sun" to set the color to "orange"
 
+// const sunColor = document.querySelectorAll('.sun');
 const sunColor = document.querySelectorAll('.sun');
-sunColor.forEach((el) => el.style.color = 'orange');
-
+sunColor.forEach((el) => el.style.color = 'orange')
 
 // Change the class of the second <li> from to "sun" to "cloudy"
 let sunToCloudy = document.getElementsByTagName('li')[1];
--- toDoList.js
+++ toDoList.js
@@ -1,68 +1,56 @@
 // If an li element is clicked, toggle the class "done" on the <li>
-// const list = document.getElementsByClassName('today-list')[0];
-// console.log(list)
 
 const list = document.querySelector('ul.today-list');
-console.log(list);
+      // console.log(list);
 
 let listItems = document.querySelectorAll('li');
-console.log(listItems);
+      // console.log(listItems);
 
-// const listChildren = list.children;
-// console.log(listChildren);
+      // const listChildren = list.children;
+      // console.log(listChildren);
 
 //toggle the class for the li element under the parent list when it is clicked
 
 list.addEventListener('click', e => {
-  console.log('clicked');
+  e.preventDefault();
+        // console.log('clicked');
   const li = e.target;
-  listItems.forEach(e => e.classList.toggle("done"));
-
-  // elements.forEach(x => x.classList.toggle('otherClass');
+  listItems.forEach(e => e.classList.toggle('done'));
+        // elements.forEach(x => x.classList.toggle('otherClass');
+        // for (let i = 0; i < listItems.length; i++) {
+        //   const li = listItems[i];
+        //   li => li.classList.toggle('done')
+        // }
 
 });
 
 // If a delete link is clicked, delete the li element / remove from the DOM
 //identify elements for button and parent of the button
 
-//get element for delete button
-const deleteButton = document.getElementsByClassName('delete')[0];
-//identiy li parent for the button element
-const buttonParent = deleteButton.parentNode
+const deleteButton = document.querySelectorAll('.delete');
+      // const buttonParent = document.querySelectorAll('.toDo-item');;
+      // const buttonUL = document.querySelectorAll('.today-list');
 
-//get ul for button
+      // console.log(deleteButton);
+      // console.log(buttonParent);
+      // console.log(buttonUL);
 
-const buttonUL = deleteButton.closest('ul.today-list');
+function deleteToDo (e) {
 
-console.log(deleteButton);
-console.log(buttonParent);
-console.log(buttonUL);
+  // Implement the click handler here for button of class 'remove'
+  var target = e.target;
+  target.parentNode.parentNode.removeChild(target.parentNode);
+};
 
-//upon click keep removing the first child of the parent until there are no more children to remove.  This insures both the span and a tags for the li are deleted
-
-
-deleteButton.addEventListener('click', (e) => {
-  while (buttonParent.firstChild) {
-    buttonParent.removeChild(buttonParent.firstChild);
-  }
-  buttonParent.remove();
-});
-
-// buttonUL.addEventListener('click', (e) => {
-//   const a = e.target;
-//   while (buttonUL.firstChild) {
-//     buttonUL.removeChild(buttonUL.firstChild);
-//   }
-// });
+for (var i = 0 ; i < deleteButton.length; i++) {
+    deleteButton[i].addEventListener("click", deleteToDo); 
+};
 
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
 // Make sure to add an event listener(s) to your new <li> (if needed)
 
-
 //identify the ul element which will be the parent of the new li
-
-
 const addListItem = function (e) {
   e.preventDefault();
   const input = this.parentNode.getElementsByTagName('input')[0];
@@ -70,6 +58,7 @@ const addListItem = function (e) {
 
   // create and add the list element
   let newToDo = document.createElement("li");
+ newToDo.setAttribute("class", "toDo-item");
 
   //append the new list to the parent ul
   list.appendChild(newToDo);

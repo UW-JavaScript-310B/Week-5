65a9a87b18dbb844f5b8bb82ea09a8eb2a0bafba
carlitos_206 <carloscaceres041@gmail.com>
2022-02-24 03:57:41 -0800

    HW complete

--- accessing.js
+++ accessing.js
@@ -1,5 +1,15 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
-
+const weatherHead = document.getElementById('weather-head');
+weatherHead.innerHTML = 'February 10 Weather Forecast, Seattle'
 // Change the styling of every element with class "sun" to set the color to "orange"
-
+    
+// To acces class it creates an array meaning that we have to loop throug it, to acces the array[i]
+let classes = document.getElementsByClassName("sun")
+console.log(classes)
+for(let i =0; i<classes.length; i++){
+    classes[i].setAttribute('style', 'color:orange')
+}
 // Change the class of the second <li> from to "sun" to "cloudy"
+// see Jquery.js file
+const child2 = document.querySelector('#weather :nth-child(2)');
+child2.setAttribute('name', 'I_also_did_it_in_Vanilla_JS')
--- wheresThePointer.js
+++ wheresThePointer.js
@@ -1,3 +1,12 @@
 // Attach one listener that will detect clicks on any of the <td>
 // elements.  Should update that element's innerHTML to be the
 // x, y coordinates of the mouse at the time of the click
+
+let box = document.querySelectorAll('td')
+for(let i =0;i<box.length;i++){
+    box[i].addEventListener('click', (e)=>{
+        let x = e.clientX
+        let y = e.clientY
+        box[i].innerHTML = `(${x} , ${y})`
+    })
+}

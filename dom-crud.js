64e589b5dc3c88b9b9e95c5a494f964192f0c443
Ryan Ku <rjhk@uw.edu>
2022-02-24 15:26:07 -0800

    finished dom-crud.js

--- dom-crud.js
+++ dom-crud.js
@@ -1,14 +1,25 @@
 // Create a new <a> element containing the text "Buy Now!" 
 // with an id of "cta" after the last <p>
+const createEl = document.createElement('a', {'id': 'cta'});
+const newText = document.createTextNode('Buy Now!');
+createEl.appendChild(newText);
 
+const targetBody = document.getElementsByTagName('body')[0];
+targetBody.appendChild(createEl);
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
-
+const carImg = document.getElementsByTagName('img')[0];
+const colorImg = carImg.dataset.color;
+console.log(colorImg);
 
 // Update the third <li> item ("Turbocharged"), 
 // set the class name to "highlight"
-
+document.getElementsByTagName('li')[2].setAttribute('class', 'highlight')
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+
+const mainEl = document.getElementsByTagName('main')[0];
+const targetP = document.getElementsByTagName('p')[0];
+mainEl.removeChild(targetP);

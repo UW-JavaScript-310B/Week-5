8f0c6f871a958fc74b202d4a6095044e10199a0e
ambz <a.bandasith@gmail.com>
2022-02-24 13:57:57 -0800

    completed homework for week-5

--- toDoList.js
+++ toDoList.js
@@ -1,14 +1,32 @@
 // If an li element is clicked, toggle the class "done" on the <li>
+const listParent = document.querySelector('ul.today-list');
+let listItems = document.querySelectorAll('li')
+
+listParent.addEventListener('click', (e) => {
+  const clickedLiElement = e.target;
+  clickedLiElement.classList.toggle('done');
+});
 
 // If a delete link is clicked, delete the li element / remove from the DOM
+const deleteEl = document.getElementsByClassName('delete')[0].parentNode;
+
+const deleteLi = (e) => {
+  deleteEl.remove()
+};
+
+deleteEl.addEventListener('click', (deleteLi));
 
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
 // Make sure to add an event listener(s) to your new <li> (if needed)
-const addListItem = function(e) {
+const addListItem = function (e) {
   e.preventDefault();
   const input = this.parentNode.getElementsByTagName('input')[0];
   const text = input.value; // use this text to create a new <li>
-
-  // Finish function here
+  let newListItem = document.createElement('li');
+  newListItem.appendChild(document.createTextNode(text));
+  listParent.appendChild(newListItem);
 };
+
+const newListItem = document.getElementsByClassName('add-item')[0];
+newListItem.addEventListener('click', addListItem);
--- traversing.js
+++ traversing.js
@@ -1,11 +1,21 @@
 // Given the <body> element as variable body,
 // access the <main> node and log to the console.
 const body = document.querySelector('body');
+const getMainNode = body.getElementsByClassName('main');
+console.log(getMainNode);
+
+// other solution
+const getMainNode1 = body.children[0];
+console.log(getMainNode1);
 
 // Given the <ul> element as variable ul,
 // access <body>  and log to the console.
 const ul = document.querySelector('ul');
+const getBody = ul.parentElement.parentElement;
+console.log(getBody);
 
 // Given the <p> element as var p,
 // access the 3rd <li>  and log to the console.
 const p = document.querySelector('p');
+const getThirdLi = p.previousElementSibling.children[2];
+console.log(getThirdLi);

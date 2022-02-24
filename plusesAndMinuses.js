a9522164d379f93b6cb20e43c0bf91de15e26ba5
Admin <admin@Admins-MacBook-Pro.local>
2022-02-24 07:26:40 -0800

    First commit

--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,16 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
 // When a user clicks the – element, the count should decrease by 1 on screen.
+
+let header = document.querySelector("h2");
+header.innerText = 0;
+
+document.getElementById("increment").addEventListener("click", addOne);
+document.getElementById("decrememt").addEventListener("click", subtractOne);
+
+function addOne() {
+  header.innerText++;
+}
+
+function subtractOne() {
+  header.innerText--;
+}
--- toDoList.js
+++ toDoList.js
@@ -1,14 +1,41 @@
 // If an li element is clicked, toggle the class "done" on the <li>
+document.querySelector("ul").addEventListener("click", updateDone);
+
+function updateDone(e) {
+  if(e.target.localName === 'li') {
+    e.target.classList.add('done');
+  }
+}
 
 // If a delete link is clicked, delete the li element / remove from the DOM
+document.querySelector("ul").addEventListener("click", deleteItem);
+
+function deleteItem(e) {
+  if(e.target.className === 'delete') {
+    e.target.parentElement.remove();
+  }
+}
 
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
 // Make sure to add an event listener(s) to your new <li> (if needed)
+
 const addListItem = function(e) {
   e.preventDefault();
   const input = this.parentNode.getElementsByTagName('input')[0];
-  const text = input.value; // use this text to create a new <li>
+  const text = input.value;
 
-  // Finish function here
+  const span = document.createElement("span");
+  span.innerText = text;
+
+  const deleteItem = document.createElement("a");
+  deleteItem.className = "delete";
+  deleteItem.innerText = "Delete";
+
+  const toDo = document.createElement("li");
+  toDo.appendChild(span);
+  toDo.appendChild(deleteItem);
+  document.getElementsByClassName("today-list")[0].appendChild(toDo);
 };
+
+document.getElementsByClassName("add-item")[0].addEventListener("click", addListItem);

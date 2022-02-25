c4a1fb2ce263efd6f866f5dfdc3b70ce9bf55dc1
Ryan Ku <rjhk@uw.edu>
2022-02-24 17:53:29 -0800

    toDoList.js

--- toDoList.js
+++ toDoList.js
@@ -1,6 +1,19 @@
 // If an li element is clicked, toggle the class "done" on the <li>
 
+const liToggle = document.getElementsByTagName('span')[0];
+
+liToggle.addEventListener('click', function(e) {
+    e.target.classList.toggle('done');
+  } );
+
 // If a delete link is clicked, delete the li element / remove from the DOM
+
+const deleteButton = document.getElementsByClassName('delete')[0];
+const deleteTarget = document.getElementsByClassName('delete')[0].parentNode;
+
+deleteButton.addEventListener('click', function(e) {
+  deleteTarget.remove(); 
+} );
 
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
@@ -11,4 +24,9 @@ const addListItem = function(e) {
   const text = input.value; // use this text to create a new <li>
 
   // Finish function here
+  const newLi = document.createElement('li');
+  newLi.appendChild(text);
 };
+
+const addList = document.getElementsByClassName('add-item')[0];
+addList.addEventListener('click', addListItem);

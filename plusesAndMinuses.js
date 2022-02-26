c2767c799c9ee9ea78f0bbd97f35bffc017c5970
Sonal Gupta <sonalaggarwal555@gmail.com>
2022-02-26 10:22:41 -0800

    Week 5 Assignment Part 5

--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,13 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
 // When a user clicks the – element, the count should decrease by 1 on screen.
+let counter = 0;
+document.getElementById('link-One').addEventListener('click',function(e) {
+    counter++;
+    console.log(`${counter}`);
+    document.getElementById("count").innerHTML = counter;
+});
+document.getElementById('link-two').addEventListener('click',function(e) {
+    counter--;
+    console.log(`${counter}`);
+    document.getElementById("count").innerHTML = counter;
+});
